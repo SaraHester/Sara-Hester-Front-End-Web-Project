49cda708e65c06ec4c9cb0ea8c25e97bd68ad721
Sara Hester <shester@basecampcodingacademy.org>
2017-11-15 13:55:47 -0600

    Added basic outline of items to sell
    Made inventory in JSON containing a list of objects.
    
    Next I plan on adding basic neccessities such as a name and navbar.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+INVENTORY = [
+    { name: 'Sword Art Online', price: 20 },
+    { name: 'Sword Art Online: season 2', price: 20 },
+    { name: 'Tokyo Ghoul', price: 20 },
+    { name: 'Tokyo Ghoul: root A', price: 20 },
+    { name: 'Attack On Titan', price: 20 },
+    { name: 'Dragon Ball', price: 20 },
+    { name: 'Dragon Ball Z', price: 20 },
+    { name: 'Dragon Ball Super', price: 20 },
+    { name: 'Dragon Ball GT', price: 20 },
+    { name: 'Naruto', price: 20 },
+    { name: 'Naruto Shippuden', price: 20 },
+    { name: 'One Piece', price: 20 },
+    { name: 'Death Note', price: 20 },
+    { name: 'Kill la Kill', price: 20 },
+    { name: 'Akame ga Kill', price: 20 },
+    { name: 'Hunter X Hunter', price: 20 },
+    { name: 'Bleach', price: 20 },
+    { name: 'My Hero Academia', price: 20 }
+];
 function main() {
     $('#game-over').html(
         'Game Over <i class="fa fa-gamepad" aria-hidden="true"></i>'
