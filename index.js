23d991966801d09272d2683c5d09c78af340498e
Sara Hester <shester@basecampcodingacademy.org>
2017-11-16 09:44:08 -0600

    Added sell function to add an item to sell.
    Added a seller property to INVENTORY items. Moved hideSellForm and showSellForm into thier own functions. Renamed previous showSellForm to toggleSellForm. Renamed the id to showSellForm as well.
    
    Next, I plan to add limits to the sell function.

--- index.js
+++ index.js
@@ -4,102 +4,144 @@ INVENTORY = [
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
     {
         name: 'Sword Art Online: season 2',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
     {
         name: 'Tokyo Ghoul',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
     {
         name: 'Tokyo Ghoul: root A',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
     {
         name: 'Attack On Titan',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
     {
         name: 'Dragon Ball',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
     {
         name: 'Dragon Ball Z',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
     {
         name: 'Dragon Ball Super',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
     {
         name: 'Dragon Ball GT',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
-    { name: 'Naruto', price: 20, inStock: 20, description: '', picUrl: '' },
+    {
+        name: 'Naruto',
+        price: 20,
+        inStock: 20,
+        description: '',
+        picUrl: '',
+        seller: 'Anime World'
+    },
     {
         name: 'Naruto Shippuden',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
-    { name: 'One Piece', price: 20, inStock: 20, description: '', picUrl: '' },
-    { name: 'Death Note', price: 20, inStock: 20, description: '', picUrl: '' },
+    {
+        name: 'One Piece',
+        price: 20,
+        inStock: 20,
+        description: '',
+        picUrl: '',
+        seller: 'Anime World'
+    },
+    {
+        name: 'Death Note',
+        price: 20,
+        inStock: 20,
+        description: '',
+        picUrl: '',
+        seller: 'Anime World'
+    },
     {
         name: 'Kill la Kill',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
     {
         name: 'Akame ga Kill',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
     {
         name: 'Hunter X Hunter',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     },
-    { name: 'Bleach', price: 20, inStock: 20, description: '', picUrl: '' },
+    {
+        name: 'Bleach',
+        price: 20,
+        inStock: 20,
+        description: '',
+        picUrl: '',
+        seller: 'Anime World'
+    },
     {
         name: 'My Hero Academia',
         price: 20,
         inStock: 20,
         description: '',
-        picUrl: ''
+        picUrl: '',
+        seller: 'Anime World'
     }
 ];
 shoppingCart = [];
@@ -110,6 +152,33 @@ function addToCart(number) {
     console.log(shoppingCart);
     drawItems();
 }
+function hideSellForm() {
+    $('#toggleSellForm').html(
+        'Add item to sell <i class="fa fa-plus" aria-hidden="true"></i>'
+    );
+    $('#toggleSellForm').css('color', 'black');
+    $('#itemName').attr('hidden', 'true');
+    $('#itemPrice').attr('hidden', 'true');
+    $('#itemUrl').attr('hidden', 'true');
+    $('#itemDescription').attr('hidden', 'true');
+    $('#itemQuantity').attr('hidden', 'true');
+    $('#addSellItem').attr('hidden', 'true');
+    $('#itemSeller').attr('hidden', 'true');
+}
+function showSellForm() {
+    $('#toggleSellForm').html(
+        'Cancel <i class="fa fa-minus" aria-hidden="true"></i>'
+    );
+    $('#toggleSellForm').css('color', 'red');
+    $('#data').html('');
+    $('#itemName').removeAttr('hidden');
+    $('#itemPrice').removeAttr('hidden');
+    $('#itemUrl').removeAttr('hidden');
+    $('#itemDescription').removeAttr('hidden');
+    $('#itemQuantity').removeAttr('hidden');
+    $('#addSellItem').removeAttr('hidden');
+    $('#itemSeller').removeAttr('hidden');
+}
 function seeInfo(number) {
     console.log('seeInfo(' + number + ') has been clicked');
     $('#data').html(
@@ -117,6 +186,8 @@ function seeInfo(number) {
             INVENTORY[number].name +
             '<br>$' +
             INVENTORY[number].price +
+            '<br>Seller: ' +
+            INVENTORY[number].seller +
             '<br>In Stock: ' +
             INVENTORY[number].inStock +
             '<br>Description : ' +
@@ -176,33 +247,42 @@ function showCart() {
     }
     $('#data').html(html + '<br>Total: ' + total);
 }
-function showSellForm() {
-    console.log($('#showSellForm').css('color'));
-    if ($('#showSellForm').css('color') === 'rgb(0, 0, 0)') {
-        $('#showSellForm').html(
-            'Cancel <i class="fa fa-minus" aria-hidden="true"></i>'
-        );
-        $('#showSellForm').css('color', 'red');
-        $('#data').html('');
-        $('#itemName').removeAttr('hidden');
-        $('#itemPrice').removeAttr('hidden');
-        $('#itemUrl').removeAttr('hidden');
-        $('#itemDescription').removeAttr('hidden');
-        $('#itemQuantity').removeAttr('hidden');
-        $('#addSellItem').removeAttr('hidden');
-    } else if ($('#showSellForm').css('color') === 'rgb(255, 0, 0)') {
-        $('#showSellForm').html(
-            'Add item to sell <i class="fa fa-plus" aria-hidden="true"></i>'
-        );
-        console.log($('#showSellForm').css('color', 'black'));
-        $('#itemName').attr('hidden', 'true');
-        $('#itemPrice').attr('hidden', 'true');
-        $('#itemUrl').attr('hidden', 'true');
-        $('#itemDescription').attr('hidden', 'true');
-        $('#itemQuantity').attr('hidden', 'true');
-        $('#addSellItem').attr('hidden', 'true');
+function toggleSellForm() {
+    if ($('#toggleSellForm').css('color') === 'rgb(0, 0, 0)') {
+        showSellForm();
+    } else if ($('#toggleSellForm').css('color') === 'rgb(255, 0, 0)') {
+        hideSellForm();
+
         drawItems();
     }
+}
+function sell() {
+    // sellers = [];
+    // names = [];
+    // for (i = 0; i < INVENTORY.length; i++) {
+    //     sellers.push(INVENTORY[i].seller);
+    //     names.push(INVENTORY[i].name);
+    // }
+    // if (names.indexOf($('#itemName').val()) > -1) {
+    //     if (sellers.indexOf($('#itemSeller').val()) > -1) {
+    //         console.log('Name and seller matches');
+    //     } else {
+    //         console.log('Name matches but seller does not match');
+    //     }
+    // } else {
+    //     console.log('Name does not match');
+    // }
+    item = {
+        name: $('#itemName').val(),
+        price: $('#itemPrice').val(),
+        inStock: $('#itemQuantity').val(),
+        description: $('#itemDescription').val(),
+        picUrl: $('#itemUrl').val(),
+        seller: $('#itemSeller').val()
+    };
+    INVENTORY.push(item);
+    hideSellForm();
+    drawItems();
 }
 function main() {
     drawItems();
